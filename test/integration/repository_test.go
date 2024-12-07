0e70e1ce0902c1fbf622b52eaee9797c2a4c747f
mizmorr <mizmorrr@gmail.com>
2024-12-07 19:23:38 +0300

    upd: some fixes tests for repo

--- test/integration/repository_test.go
+++ test/integration/repository_test.go
@@ -5,15 +5,19 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	logger "github.com/mizmorr/loggerm"
 	"github.com/mizmorr/wallet/internal/model"
 	reporitory "github.com/mizmorr/wallet/internal/repository"
+	"github.com/mizmorr/wallet/pkg/types"
 	"github.com/mizmorr/wallet/store/pg"
 	"github.com/stretchr/testify/assert"
 )
 
+const loggerKey types.ContextKey = "logger"
+
 func TestNewWalletRepository(t *testing.T) {
 	// Setup
-	ctx := context.Background()
+	ctx := context.WithValue(context.Background(), loggerKey, logger.Get("debug"))
 	db, err := pg.Dial(ctx)
 	assert.NoError(t, err)
 	// Test
@@ -22,10 +26,10 @@ func TestNewWalletRepository(t *testing.T) {
 	assert.NotNil(t, repo)
 }
 
-func TestUpsert(t *testing.T) {
+func TestDeposit(t *testing.T) {
 	// Setup
-	ctx := context.Background()
-
+	var expectedAmount int64 = 200
+	ctx := context.WithValue(context.Background(), loggerKey, logger.Get("debug"))
 	db, err := pg.Dial(ctx)
 	assert.NoError(t, err)
 
@@ -34,23 +38,67 @@ func TestUpsert(t *testing.T) {
 
 	id := uuid.New()
 	wallet := &model.Wallet{
-		ID:        id,
-		Operation: "Deposit",
-		Amount:    100,
+		ID:     id,
+		Amount: 100,
 	}
-	// Test
-	returnedId, err := repo.Upsert(ctx, wallet)
+
+	_, err = repo.Create(ctx, wallet)
 	assert.NoError(t, err)
-	assert.NotNil(t, id)
-	assert.Equal(t, id, returnedId)
+
+	// Test
+	err = repo.Deposit(ctx, wallet)
+	assert.NoError(t, err)
+
 	// Verify
+	walletNew, err := repo.GetByID(ctx, id)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedAmount, walletNew.Amount)
+
+	// Clean up
+	_, err = db.Exec(context.Background(), "DELETE FROM wallets WHERE id=$1", id)
+	assert.NoError(t, err)
+}
+
+func TestWithdraw(t *testing.T) {
+	// Setup
+	var expectedAmount int64 = 50
+	ctx := context.WithValue(context.Background(), loggerKey, logger.Get("debug"))
+	db, err := pg.Dial(ctx)
+	assert.NoError(t, err)
+
+	repo := reporitory.NewWalletRepository(db)
+	assert.NotNil(t, repo)
+
+	id := uuid.New()
+	wallet := &model.Wallet{
+		ID:     id,
+		Amount: 100,
+	}
+	walletForWithdraw := &model.Wallet{
+		ID:     id,
+		Amount: 50,
+	}
+	_, err = repo.Create(ctx, wallet)
+	assert.NoError(t, err)
+
+	// Test
+	err = repo.Withdraw(ctx, walletForWithdraw)
+	assert.NoError(t, err)
+
+	// Verify
+	walletNew, err := repo.GetByID(ctx, id)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedAmount, walletNew.Amount)
+
+	// Clean up
 	_, err = db.Exec(context.Background(), "DELETE FROM wallets WHERE id=$1", id)
 	assert.NoError(t, err)
 }
 
 func TestGetByID(t *testing.T) {
 	// Setup
-	ctx := context.Background()
+
+	ctx := context.WithValue(context.Background(), loggerKey, logger.Get("debug"))
 
 	db, err := pg.Dial(ctx)
 	assert.NoError(t, err)
@@ -60,21 +108,52 @@ func TestGetByID(t *testing.T) {
 
 	id := uuid.New()
 	wallet := &model.Wallet{
-		ID:        id,
-		Operation: "Deposit",
-		Amount:    100,
+		ID:     id,
+		Amount: 100,
 	}
-	// Test
-	returnedId, err := repo.Upsert(ctx, wallet)
+	returnedId, err := repo.Create(ctx, wallet)
 	assert.NoError(t, err)
 	assert.NotNil(t, id)
 	assert.Equal(t, id, returnedId)
 
+	// Test
 	walletNew, err := repo.GetByID(ctx, id)
+
+	// Verify
 	assert.NoError(t, err)
 	assert.Equal(t, wallet, walletNew)
 
-	// Verify
+	// Clean up
+	_, err = db.Exec(context.Background(), "DELETE FROM wallets WHERE id=$1", id)
+	assert.NoError(t, err)
+}
+
+func TestWithdrawFailed(t *testing.T) {
+	// Setup
+	ctx := context.WithValue(context.Background(), loggerKey, logger.Get("debug"))
+	db, err := pg.Dial(ctx)
+	assert.NoError(t, err)
+
+	repo := reporitory.NewWalletRepository(db)
+	assert.NotNil(t, repo)
+
+	id := uuid.New()
+	wallet := &model.Wallet{
+		ID:     id,
+		Amount: 100,
+	}
+	walletForWithdraw := &model.Wallet{
+		ID:     id,
+		Amount: 150,
+	}
+	_, err = repo.Create(ctx, wallet)
+	assert.NoError(t, err)
+
+	// Test
+	err = repo.Withdraw(ctx, walletForWithdraw)
+	assert.Error(t, err)
+
+	// Clean up
 	_, err = db.Exec(context.Background(), "DELETE FROM wallets WHERE id=$1", id)
 	assert.NoError(t, err)
 }
