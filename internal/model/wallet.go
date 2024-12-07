b645ccef814d28c09e324e457c35d0a910702680
mizmorr <mizmorrr@gmail.com>
2024-12-07 19:17:47 +0300

    upd: fixed models

--- internal/model/wallet.go
+++ internal/model/wallet.go
@@ -3,25 +3,31 @@ package model
 import "github.com/google/uuid"
 
 type Wallet struct {
-	ID        uuid.UUID `pg:"id, notnull, pk"`
-	Amount    int64     `pg:"amount, notnull"`
-	Operation string    `pg:"operation, notnull"`
+	ID     uuid.UUID `pg:"id, notnull, pk"`
+	Amount int64     `pg:"amount, notnull"`
 }
 
 type WalletRequest struct {
-	ID uuid.UUID `json:"id"`
+	ID        uuid.UUID `json:"id" binding:"required"`
+	Amount    int64     `json:"amount" validate:"gt=0"`
+	Operation string    `json:"operation" validate:"oneof=deposit withdraw"`
 }
 
-type WalletWeb struct {
-	ID        uuid.UUID `json:"id"`
-	Amount    int64     `json:"amount"`
-	Operation string    `json:"operation"`
+type WalletResponse struct {
+	ID     uuid.UUID `json:"id"`
+	Amount int64     `json:"amount"`
 }
 
-func (w *Wallet) ToWeb() *WalletWeb {
-	return &WalletWeb{
-		ID:        w.ID,
-		Amount:    w.Amount,
-		Operation: w.Operation,
+func (w *Wallet) ToWeb() *WalletResponse {
+	return &WalletResponse{
+		ID:     w.ID,
+		Amount: w.Amount,
+	}
+}
+
+func (w *WalletRequest) ToDB() *Wallet {
+	return &Wallet{
+		ID:     w.ID,
+		Amount: w.Amount,
 	}
 }
