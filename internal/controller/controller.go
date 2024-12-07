872a3b38ba8e84480d4b62178ac378a5f924288c
mizmorr <mizmorrr@gmail.com>
2024-12-07 19:22:44 +0300

    upd: add some validation

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -2,24 +2,91 @@ package controller
 
 import (
 	"context"
-	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/mizmorr/wallet/config"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+	"github.com/mizmorr/wallet/internal/model"
+	"github.com/mizmorr/wallet/internal/service"
+	"github.com/mizmorr/wallet/pkg/apperror"
 )
 
-func ToPostgres() error {
-	ctx := context.Background()
-	confg := config.Get()
-	poolConfig, err := pgxpool.ParseConfig(confg.DatabaseURL)
+type Service interface {
+	Deposit(ctx context.Context, wallet *model.WalletRequest) error
+	Get(ctx context.Context, id uuid.UUID) (*model.WalletResponse, error)
+	Withdraw(ctx context.Context, wallet *model.WalletRequest) error
+}
 
-	db, err := pgxpool.NewWithConfig(ctx, poolConfig)
-	var greeting string
-	err = db.QueryRow(context.Background(), "SELECT 'Hello, world!'").Scan(&greeting)
-	if err != nil {
-		return err
+var (
+	_     Service = (*service.WalletService)(nil)
+	valid         = validator.New()
+)
+
+type WalletController struct {
+	ctx context.Context
+	svc Service
+}
+
+func NewWalletController(svc Service, ctx context.Context) *WalletController {
+	return &WalletController{
+		svc: svc,
+		ctx: ctx,
+	}
+}
+
+func (c *WalletController) Get(g *gin.Context) {
+	userid_raw, ok := g.Params.Get("id")
+	if !ok {
+		respondWithError(g, apperror.ErrBadRequest, "No id provided")
+		return
 	}
 
-	fmt.Println(greeting)
-	return nil
+	userid, err := uuid.Parse(userid_raw)
+	if err != nil {
+		respondWithError(g, apperror.ErrValidation, "Could not parse userid")
+		return
+	}
+
+	user, err := c.svc.Get(c.ctx, userid)
+	if err != nil {
+		respondWithError(g, apperror.ErrNotFound, "Could not find user with that id")
+		return
+	}
+	g.JSON(http.StatusOK, user)
+}
+
+func (c *WalletController) Operate(g *gin.Context) {
+	walletToOperate := &model.WalletRequest{}
+
+	err := g.Bind(walletToOperate)
+	if err != nil {
+		log.Println(walletToOperate)
+		respondWithError(g, apperror.ErrBadRequest, "Invalid ID!")
+		return
+	}
+	err = valid.Struct(walletToOperate)
+	if err != nil {
+		respondWithError(g, apperror.ErrValidation, err.Error())
+		return
+	}
+
+	switch walletToOperate.Operation {
+	case "deposit":
+		err = c.svc.Deposit(c.ctx, walletToOperate)
+	case "withdraw":
+		err = c.svc.Withdraw(c.ctx, walletToOperate)
+	}
+	if err != nil {
+		respondWithError(g, apperror.ErrForbidden, err.Error())
+		return
+	}
+
+	g.JSON(http.StatusOK, "Success!")
+}
+
+func respondWithError(g *gin.Context, err apperror.AppError, details string) {
+	err.Details = details
+	g.AbortWithStatusJSON(err.StatusCode, err)
 }
