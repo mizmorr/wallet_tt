76f2ff3579501279267f1765e680f701381d5a2e
mizmorr <mizmorrr@gmail.com>
2024-12-07 03:55:46 +0300

    upd: make repo layer

--- internal/repository/wallet.go
+++ internal/repository/wallet.go
@@ -1,6 +1,13 @@
 package reporitory
 
-import "github.com/mizmorr/wallet/store/pg"
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/mizmorr/wallet/internal/model"
+	"github.com/mizmorr/wallet/store/pg"
+)
 
 type WalletRepository struct {
 	db *pg.DB
@@ -8,4 +15,35 @@ type WalletRepository struct {
 
 func NewWalletRepository(db *pg.DB) *WalletRepository {
 	return &WalletRepository{db: db}
+}
+
+func (repo *WalletRepository) Upsert(ctx context.Context, wallet *model.Wallet) (uuid.UUID, error) {
+	query := `INSERT INTO wallets (id, operation, amount)
+		VALUES ($1, $2, $3)
+		ON CONFLICT (id)
+		DO UPDATE SET operation = COALESCE(NULLIF(EXCLUDED.operation,''), wallets.operation), amount = EXCLUDED.amount;
+	`
+	_, err := repo.db.Exec(context.Background(), query, wallet.ID, wallet.Operation, wallet.Amount)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return wallet.ID, nil
+}
+
+func (repo *WalletRepository) GetByID(ctx context.Context, id uuid.UUID) (*model.Wallet, error) {
+	var (
+		query  = `SELECT id,operation,amount FROM wallets WHERE id = $1`
+		wallet model.Wallet
+	)
+
+	err := repo.db.QueryRow(context.Background(), query, id).Scan(&wallet.ID, &wallet.Operation, &wallet.Amount)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &wallet, nil
 }
